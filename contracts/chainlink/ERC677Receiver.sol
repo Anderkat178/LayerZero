0b40e8347674ca5174625ae4f383a3fae52b43cc
Anderkat178 <108509495+Anderkat178@users.noreply.github.com>
2023-11-20 11:58:09 +0200

    Update ERC677Receiver.sol

--- contracts/chainlink/ERC677Receiver.sol
+++ contracts/chainlink/ERC677Receiver.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-pragma solidity 0.7.6;
+pragma solidity 0.7.11;
 
 interface ERC677Receiver {
     function onTokenTransfer(address _sender, uint _value, bytes memory _data) external;
